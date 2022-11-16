d3f0e7ba56755f25cc19d6b4a8193aaba1b38caa
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-11-16 20:30:28 -0300

    logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Container, Logo, Button } from "./style";
 import carImg from "../../assets/car.svg";
 import * as Dialog from "@radix-ui/react-dialog";
-import { Trigger} from "./button";
+import { Trigger } from "./button";
 
 function Header() {
   const totalQuality: any = useSelector<unknown | any>(
@@ -10,9 +10,7 @@ function Header() {
   );
   return (
     <Container>
-      <Logo>
-        MKS <span>Sistemas</span>
-      </Logo>
+      <Logo>Logo</Logo>
       <Trigger>
         <Button>
           <img src={carImg} alt="" />
